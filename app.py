602e5354cb8327ac967c6c1e121a3fcb9de94111
Ni4ka1991 <astahova_nica@mail.ru>
2021-05-07 14:48:08 +0300

    Add file app.py

--- app.py
+++ app.py
@@ -3,21 +3,22 @@
 from os import system
 
 class FruitBox:
-    def __init__( self, apples, oranges, err = "error" ):
+    def __init__( self, apples, oranges ):
+        
         
         if ((apples + oranges) > 50 ):
-            self.apples = err
-            self.oranges = err
+            print("Too much fruits in box")
+            input( "hit Enter to continue ..." )
         else:
             if ( type(apples) == int ):
                 self.apples = apples
             else:
-                self.apples = err
+                print("Enter an integer quantity!")
         
             if ( type(oranges) == int ):
                 self.oranges = oranges
             else:
-                self.oranges = err
+                print("Enter an integer quantity!")
     
     def __str__(self):
         return f"In box: Apples: {self.apples} & Oranges: {self.oranges}"
@@ -25,12 +26,12 @@ class FruitBox:
     def __add__( self, other_box ):
         return FruitBox(self.apples + other_box.apples, self.oranges + other_box.oranges)
 
-a_box = FruitBox( 3, 1, "ERROR" )
-b_box = FruitBox( 5, 2, "ERROR" )
+a_box = FruitBox( 50, 1 )
+b_box = FruitBox( 5, 2.2 )
 
-result_box = a_box + b_box
-str(result_box)
+#result_box = a_box + b_box
 system( "clear" )
-print(result_box)
+print( a_box )
+#print ( b_box )
 
 
